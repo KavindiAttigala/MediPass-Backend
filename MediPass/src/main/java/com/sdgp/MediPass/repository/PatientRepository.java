4fb9be01eb90e839c205139568dcf8a5ad442e5a
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-02-25 16:39:17 +0530

    Project Files

--- MediPass/src/main/java/com/sdgp/MediPass/repository/PatientRepository.java
+++ MediPass/src/main/java/com/sdgp/MediPass/repository/PatientRepository.java
@@ -2,13 +2,10 @@ package com.sdgp.MediPass.repository;
 
 import com.sdgp.MediPass.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     List<Patient> findByEmail(String email);
     List<Patient> findByNic(String nic);
-    Optional<Patient> findByNicAndMediId(String nic, String mediId);
 }
