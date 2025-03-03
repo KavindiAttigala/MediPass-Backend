2569df72e8fa508bfc12a3358d605bf236b9adbd
KavindiAttigala <164112883+KavindiAttigala@users.noreply.github.com>
2025-03-03 11:50:55 +0530

    medicalReports

--- MediPass/src/main/java/com/sdgp/MediPass/model/MedicalReports.java
+++ MediPass/src/main/java/com/sdgp/MediPass/model/MedicalReports.java
@@ -1,12 +1,16 @@
 package com.sdgp.MediPass.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 public class MedicalReports {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long medicalNotes;
 
     @ManyToOne
-    @JoinColumn (name="mediIDnotes", nullable = false)
+    @JoinColumn(name="mediIDnotes", nullable = false)
     private Patient patient;
 
     private LocalDate date;
@@ -18,4 +22,92 @@ public class MedicalReports {
     private String fileName;
     private String fileType;
     private String filePath;
+
+    public Long getMedicalNotes() {
+        return medicalNotes;
+    }
+
+    public void setMedicalNotes(Long medicalNotes) {
+        this.medicalNotes = medicalNotes;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDocName() {
+        return docName;
+    }
+
+    public void setDocName(String docName) {
+        this.docName = docName;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getSlmc() {
+        return slmc;
+    }
+
+    public void setSlmc(String slmc) {
+        this.slmc = slmc;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public void setTextContent(String textContent) {
+        this.textContent = textContent;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
 }
