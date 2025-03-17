19052f12469f013d6168c936f787d824e81f602f
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-03-17 21:28:03 +0530

    Update Patient.java

--- MediPass/src/main/java/com/sdgp/MediPass/model/Patient.java
+++ MediPass/src/main/java/com/sdgp/MediPass/model/Patient.java
@@ -22,8 +22,8 @@ public class Patient {
     private String password;
     private String role;
 
-    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
-    private List<CalendarReminder> reminders;
+//    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
+//    private List<CalendarReminder> reminders;
 
     public Patient(String firstName, String lastName, String email, String nic, String contactNumber, String password) {
         this.firstName = firstName;
