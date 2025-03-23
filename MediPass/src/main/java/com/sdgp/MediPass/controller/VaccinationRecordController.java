090ed152b8dcecb4635bcf21f80a42eb4d213450
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-03-23 17:39:17 +0530

    Mapping updates

--- MediPass/src/main/java/com/sdgp/MediPass/controller/VaccinationRecordController.java
+++ MediPass/src/main/java/com/sdgp/MediPass/controller/VaccinationRecordController.java
@@ -20,14 +20,14 @@ public class VaccinationRecordController {
     private VaccinationRecordService vaccinationRecordService;
 
     @ApiOperation(value = "Storing vaccination records")
-    @PostMapping
+    @PostMapping("/add-V-records")
     public ResponseEntity<VaccinationRecords> addVaccinationRecords(@RequestBody VaccinationRecords bloodDonation){
         VaccinationRecords savedRecords= vaccinationRecordService.addVaccinationRecords(bloodDonation);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedRecords);
     }
 
     @ApiOperation(value = "Retrieving vaccination records")
-    @GetMapping
+    @GetMapping("/get-V-records")
     public ResponseEntity<List<VaccinationRecords>> getAllVaccinationRecords(){
         return ResponseEntity.ok(vaccinationRecordService.getAllVaccinationRecords());
     }
