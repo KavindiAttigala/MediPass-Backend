3b055f3ee68ce02ba710a2559a0f4b3bab9094df
Lochana Wijesinghe <samadhi.20230497@iit.ac.lk>
2025-04-03 14:30:56 +0530

    Update GuestDoctorService.java

--- MediPass/src/main/java/com/sdgp/MediPass/service/GuestDoctorService.java
+++ MediPass/src/main/java/com/sdgp/MediPass/service/GuestDoctorService.java
@@ -5,39 +5,12 @@ import com.sdgp.MediPass.repository.GuestDoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class GuestDoctorService {
     @Autowired
     private GuestDoctorRepository guestDoctorRepo;
 
-    private OTPService otpService;
-
-    //store doctor details temporarily
-    private final Map<Long, Boolean> otpStatus = new HashMap<>();
-
-    public boolean verifyOtpForMediId(long mediId, String otp) {
-        boolean isValid = otpService.verifyOTP(mediId, otp);
-        if (isValid) {
-            otpStatus.put(mediId, true); // Mark as verified
-        }
-        return isValid;
+    public GuestDoctor saveDoctor(GuestDoctor guestDoctor){
+        return guestDoctorRepo.save(guestDoctor);
     }
-
-    public GuestDoctor saveDoctorDetails(long mediId, GuestDoctor guestDoctor) {
-        if (!otpStatus.getOrDefault(mediId, false)) {
-            throw new RuntimeException("OTP verification required before saving doctor details");
-        }
-
-        //Store doctor details in the database
-        GuestDoctor savedDoctor = guestDoctorRepo.save(guestDoctor);
-        otpStatus.remove(mediId);
-        return savedDoctor;
-    }
-
-//    public GuestDoctor saveDoctor(GuestDoctor guestDoctor){
-//        return guestDoctorRepo.save(guestDoctor);
-//    }
 }
